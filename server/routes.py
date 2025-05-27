d47ce3f3c1bdbf877d0269e9c314068ff7966110
Amira-Abbas <158269019+amira-abbas@users.noreply.github.com>
2025-05-27 17:22:26 +0300

    Update routes.pyee

--- server/routes.py
+++ server/routes.py
@@ -1,4 +1,3 @@
-
 from flask import request, render_template, make_response
 
 from server.webapp import flaskapp, cursor
@@ -12,19 +11,21 @@ def index():
     read = bool(request.args.get('read'))
 
     if name:
-         "SELECT * FROM books WHERE name LIKE %s",
-          ('%' + name + '%',)
+        cursor.execute(
+            "SELECT * FROM books WHERE name LIKE %s",
+            ('%' + name + '%',)
         )
         books = [Book(*row) for row in cursor]
 
     elif author:
         cursor.execute(
-            "SELECT * FROM books WHERE author LIKE %s", author
+            "SELECT * FROM books WHERE author LIKE %s",
+            ('%' + author + '%',)
         )
         books = [Book(*row) for row in cursor]
 
     else:
         cursor.execute("SELECT name, author, read FROM books")
         books = [Book(*row) for row in cursor]
-        
+
     return render_template('books.html', books=books)
